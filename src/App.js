5022ef9cc290d8af04c1771d469acaacd16eb737
magdalenamucaj <48995297+magdalenamucaj@users.noreply.github.com>
2022-05-18 16:14:07 +0200

    signin/signup forms

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
         <Route path='/' element={<Explore />}/>
         <Route path='/offers' element={<Offers />}/>
         <Route path='/profile' element={<SignIn />}/>
-        <Route path='/signIn' element={<SignIn />}/>
-        <Route path='/signUp' element={<SignUp />}/>
+        <Route path='/sign-in' element={<SignIn />}/>
+        <Route path='/sign-up' element={<SignUp />}/>
         <Route path='/forgotPassword' element={<ForgotPassword />}/>
       </Routes>
       <Navbar />
